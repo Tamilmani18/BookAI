cdb8d113db84a9fcaec19ae5088f7d6e80b45970
Tamil <Tamilmani.tm018@gmail.com>
2024-08-16 16:39:59 +0530

    added dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { IoMoon, IoSunny } from "react-icons/io5";
 
 const Header = () => {
+  const [dark, setDark] = React.useState(false);
+
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
+
   return (
-    <div className="bg-[#6366f1] h-32 flex justify-center items-center">
+    <div className="bg-[#6366f1] h-32 flex justify-between items-center px-4 dark:bg-black">
+      <div className="flex-1 flex justify-center">
+        <p className="text-5xl font-bold text-white dark:text-gray-200">
+          Book Generator API
+        </p>
+      </div>
       <div>
-        <p className="text-5xl font-bold text-white">Book Generator API</p>
+        <button
+          onClick={() => darkModeHandler()}
+          className="bg-white text-black dark:bg-black dark:text-white font-bold py-2 px-4 rounded"
+        >
+          {dark && <IoSunny />}
+          {!dark && <IoMoon />}
+        </button>
       </div>
     </div>
   );
